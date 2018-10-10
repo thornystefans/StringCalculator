53415694f626da1a9aa9b008b01f083f6b74a3f7
thornystefans <thorny17@ru.is>
2018-10-10 20:11:52 +0000

    Implemented add for two numbers

--- src/calculator.js
+++ src/calculator.js
@@ -3,6 +3,12 @@ function add(number) {
 	if(number == "") {
 		return 0;
 	}
+
+	if(number.includes(",")) {
+		var numberArray = number.split(",");
+		return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+	}
+
 	return parseInt(number);
 }
 
